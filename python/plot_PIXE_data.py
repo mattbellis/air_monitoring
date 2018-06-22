f712a8b2824ff01852b8b2ba72b07d10056def1b
mattbellis <matthew.bellis@gmail.com>
2018-06-22 00:02:45 -0400

    Stuff.

---
+++ python/plot_PIXE_data.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+import sys
+
+infilenames = sys.argv[1:]
+
+plt.figure()
+
+for infilename in infilenames:
+    data = open(infilename,encoding = "ISO-8859-1").readlines()
+
+    live_time = float(data[7].split()[-1])
+    print(live_time)
+
+    #print(data[16:1040])
+    vals = []
+    for i in data[16:1040]:
+        vals.append(float(i.strip()))
+
+    vals = np.array(vals)
+    vals /= live_time
+    x = np.linspace(0,len(vals),len(vals))
+    #print(x)
+    x*=2.8
+
+    plt.plot(x,vals,'-',label=infilename)
+
+plt.yscale('log')
+plt.legend()
+
+plt.show()
--- python/plot_data.py
+++ python/plot_data.py
@@ -8,10 +8,23 @@ infile = open(infilename,'r')
 
 vel = []
 for line in infile:
-    print(line)
+    #print(line)
     vals = line.split()
-    if len(vals)==7:
-        vel.append(float(vals[3]))
+    if len(vals)>=7:
+        V = float(vals[1])
+        offset = float(vals[0])
+
+        sig = V*2.5/1024.0 
+        dP = sig*1000
+        print(dP, " Pa")
+
+
+        #dV = V-offset
+        #dP = 1000*dV
+        v = np.sqrt(dP*2/1.225)
+        #print(v,float(vals[4]))
+        #vel.append(float(vals[4]))
+        vel.append(v)
 
 time = np.linspace(0,2*len(vel),len(vel))
 vel = np.array(vel).astype(float)
@@ -19,7 +32,7 @@ vel = np.array(vel).astype(float)
 time = time[vel==vel]
 vel = vel[vel==vel]
 
-print(vel)
+#print(vel)
 
 
 plt.figure()
