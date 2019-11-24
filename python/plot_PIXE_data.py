58ff48cea58c5dd909870b9dbf5c6782d94db1a2
mattbellis <matthew.bellis@gmail.com>
2019-11-24 15:37:34 -0500

    Stuff.

--- python/plot_PIXE_data.py
+++ python/plot_PIXE_data.py
@@ -10,8 +10,23 @@ plt.figure(figsize=(12,5))
 for icount,infilename in enumerate(infilenames):
     data = open(infilename,encoding = "ISO-8859-1").readlines()
 
+    filelabel = infilename.split('/')[-1]
+
     live_time = float(data[7].split()[-1])
     print(live_time)
+
+    # Calibration
+    calib_labels = np.array([int(data[13].split()[0]), int(data[14].split()[0])])
+    print(calib_labels)
+    calib_channels = np.array([float(data[13].split()[-1]), float(data[14].split()[-1])])
+    print(calib_channels)
+
+    slope = (calib_channels[1] -  calib_channels[0])/(calib_labels[1] - calib_labels[0])
+    intercept1 = calib_channels[1] - calib_labels[1]*slope
+    intercept0 = calib_channels[0] - calib_labels[0]*slope
+    print(slope,intercept1,intercept0)
+    intercept = (intercept1 + intercept0)/2
+
 
     #print(data[16:1040])
     vals = []
@@ -22,20 +37,27 @@ for icount,infilename in enumerate(infilenames):
     vals /= live_time
     x = np.linspace(0,len(vals),len(vals))
     #print(x)
-    x*=0.028
+    #x*=0.028
+    x *= slope
+    x += intercept
 
     #plt.plot(x,vals,'.-',label=infilename,linewidth=3)
     # For paper plot
     # python plot_PIXE_data.py PIXE_data/Ktape_*
+    plt.plot(x,vals,'-',linewidth=3,label=filelabel)
+    '''
     if icount%2==0:
         plt.plot(x,vals,'-',linewidth=3,label='Kapton tape, 0.8 micron filter')
     else:
         plt.plot(x,vals,'--',linewidth=3,label='Kapton tape, 2.5 micron filter')
+    '''
 
 plt.yscale('log')
-plt.xlim(2,5)
+#plt.xlim(2,5)
 #plt.xlim(2,10)
-plt.ylim(1,140)
+#plt.xlim(2,15)
+#plt.xlim(10,15)
+#plt.ylim(0.01,140)
 plt.xlabel('Energy [keV]',fontsize=18)
 plt.ylabel('Counts',fontsize=18)
 ax = plt.gca()
@@ -43,7 +65,8 @@ ax.tick_params(axis = 'both', which = 'major', labelsize = 18)
 
 plt.tight_layout()
 
-plt.legend(loc='lower left',fontsize=18)
+#plt.legend(loc='lower left',fontsize=18)
+plt.legend()
 
 plt.savefig('pixe_spectrum.png')
 
